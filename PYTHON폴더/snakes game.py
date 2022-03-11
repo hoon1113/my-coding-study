88051bd2af7cc54f6fea3533557255821932409b
hoon1113 <tree4748@naver.com>
2022-03-11 15:42:57 +0900

    설명추가

--- PYTHON폴더/snakes game.py
+++ PYTHON폴더/snakes game.py
@@ -28,7 +28,9 @@ def create_snake(pos):
     snake_body = Turtle()
     snake_body.shape("square")
     snake_body.color("white")
+    #지나가는 자리 표시 안되게 함(팬을 들고 이동)
     snake_body.up()
+    #이동
     snake_body.goto(pos)
     snakes.append(snake_body)
 
@@ -38,12 +40,14 @@ def rand_pos():
     rand_y = random.randint(-250,250)
     return rand_x, rand_y
 
+#점수 함수
 def score_update():
     global score
     score +=1
     score_pen.clear()
     score_pen.write(f"점수 : {score}", font=("", 15, "bold"))
 
+#게임 끝났을때 나오는 글
 def game_over():
     score_pen.goto(0,0)
     score_pen.write("Game Over", False, "center", ("", 30, "bold"))
@@ -53,14 +57,17 @@ screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("green")
 screen.title("Snake Game")
+#화면 깜빡임 방지
 screen.tracer(0)
 
-# 뱀 만들기
+#처음 기본 뱀 모양
 start_pos = [(0,0),(-20,0),(-40,0)]
+#게임을 하면서 바뀔 뱀 모양
 snakes = []
 #점수초기값
 score = 0
 
+#처음 기본뱀 길이 만큼 뱀모양 만들기
 for pos in start_pos:
     create_snake(pos)
 
@@ -74,6 +81,7 @@ food.goto(rand_pos())
 
 #점수
 score_pen = Turtle()
+#모양 숨김
 score_pen.ht()
 score_pen.up()
 score_pen.goto(-270,250)
@@ -85,28 +93,35 @@ screen.onkeypress(up,"Up")
 screen.onkeypress(down,"Down")
 screen.onkeypress(right,"Right")
 screen.onkeypress(left,"Left")
-#뱀 움직임
+#게임 실행
 game_on = True
+#뱀 움직임
 while game_on:
     screen.update()
     time.sleep(0.07)
+    #뱀이 움직일때 머리따라 몸이 움직이게 함
     for i in range(len(snakes) -1, 0, -1):
         snakes[i].goto(snakes[i-1].pos())
 
+    #뱀머리 15만큼 움직임
     snakes[0].forward(15)
 
+    #만약 뱀머리가 먹이와의 거리가 15밑일때 점수추가 꼬리하나 늘어남(리스트 하나 추가)
     if snakes[0].distance(food) < 15:
         score_update()
 
         food.goto(rand_pos())
         create_snake(snakes[-1].pos())
     
+    #뱀이 벽에 대였을때 종료
     if snakes[0].xcor() >280 or snakes[0].xcor() < -280 or snakes[0].ycor() >280 or snakes[0].ycor() < -280:
         game_on = False
         game_over()
 
+    #뱀이 머리가아닌 몸에 대였을때 종료
     for body in snakes[1:]:
         if snakes[0].distance(body) < 10:
             game_on = False
             game_over()
+#화면 꺼지지않게 방지
 screen.mainloop()
